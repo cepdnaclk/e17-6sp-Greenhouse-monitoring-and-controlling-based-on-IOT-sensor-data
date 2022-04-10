978c6962a289f2fb7e61537a1f87119deacd6bc0
IshiniUdara <e17312@eng.pdn.ac.lk>
2022-04-10 12:47:01 +0530

    Reports page updated

--- Frontend/coreui-free-react-admin-template/src/routes.js
+++ Frontend/coreui-free-react-admin-template/src/routes.js
@@ -50,6 +50,9 @@ const Toasts = React.lazy(() => import('./views/notifications/toasts/Toasts'))
 
 const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
 
+//UIs
+const reports = React.lazy(() => import('./views/reports'))
+
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
@@ -95,6 +98,7 @@ const routes = [
   { path: '/notifications/modals', name: 'Modals', element: Modals },
   { path: '/notifications/toasts', name: 'Toasts', element: Toasts },
   { path: '/widgets', name: 'Widgets', element: Widgets },
+  { path: '/reports', name: 'Reports', element: reports },
 ]
 
 export default routes
